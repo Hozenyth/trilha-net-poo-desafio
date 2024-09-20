80ce80bb644c933379cf6160bc03998dd17d25d7
hozenyth.andrade <hozenyth.andrade@gmail.com>
2024-09-20 11:11:27 -0300

    desdafio DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Testes Smartphone Nokia");
+
+var nokia = new Nokia { Numero = "123456" , IMEI = "12345", Memoria = 64, Modelo = "abc123"};
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+
+var iphone = new Iphone { Numero = "4321", IMEI = "12345", Memoria = 64, Modelo = "1234FGH" };
